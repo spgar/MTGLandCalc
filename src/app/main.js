b02040867439e85c6c580a6780c95c6b6b4e8e90
spgar <steve.gargolinski@gmail.com>
2014-07-30 15:53:34 -0400

    Basic first-pass implementation

--- src/app/main.js
+++ src/app/main.js
@@ -20,15 +20,70 @@
 define([ 'dojo/has', 'require' ], function (has, require) {
 	var app = {};
 
-    /* This only runs in the browser. */
+	function getConstant() {
+		return 1;
+	}
+
+	/* This only runs in the browser. */
 	if (has('host-browser')) {
-		require([ './widgets/ManaSymbolQuantity', 'dojo/domReady!' ], function (ManaSymbolQuantity)
-        {
-            app.msWhite = new ManaSymbolQuantity({color: 'White'}).placeAt(document.body);
-            app.msWhite.startup();
-            
-            app.msBlue = new ManaSymbolQuantity({color: 'Blue'}).placeAt(document.body);
-            app.msBlue.startup();
+		require(['dojo/dom', './widgets/ManaSymbolQuantity', 'dijit/Dialog', 'dijit/form/NumberSpinner', 'dojo/domReady!'], function(dom, ManaSymbolQuantity, Dialog)
+		{
+			/* Initialize the 5 mana symbol quantity widgets */
+			app.msWhite = new ManaSymbolQuantity({color: 'W'}).placeAt(document.body);
+			app.msBlue = new ManaSymbolQuantity({ color: 'U' }).placeAt(document.body);
+			app.msBlack = new ManaSymbolQuantity({ color: 'B' }).placeAt(document.body);
+			app.msRed = new ManaSymbolQuantity({ color: 'R' }).placeAt(document.body);
+			app.msGreen = new ManaSymbolQuantity({ color: 'G' }).placeAt(document.body);
+
+			app.msWhite.startup();
+			app.msBlue.startup();
+			app.msBlack.startup();
+			app.msRed.startup();
+			app.msGreen.startup();
+
+			var submitButton = dom.byId("submitButton");
+			dojo.connect(submitButton, "onclick", function(evt) {
+				/* Figure out all of the mana symbol quantities that we're dealing with. */
+				var w = app.msWhite.getSingleSymbolQuantity();
+				var ww = app.msWhite.getDoubleSymbolQuantity();
+				var u = app.msBlue.getSingleSymbolQuantity();
+				var uu = app.msBlue.getDoubleSymbolQuantity();
+				var b = app.msBlack.getSingleSymbolQuantity();
+				var bb = app.msBlack.getDoubleSymbolQuantity();
+				var r = app.msRed.getSingleSymbolQuantity();
+				var rr = app.msRed.getDoubleSymbolQuantity();
+				var g = app.msGreen.getSingleSymbolQuantity();
+				var gg = app.msGreen.getDoubleSymbolQuantity();
+
+				var totalSymbols = w + ww + u + uu + b + bb + r + rr + g + gg;
+				if (totalSymbols === 0) {
+					var noSymbolDialog = new Dialog({
+						title: "Invalid Input",
+						content: "Add at least one symbol."
+					});
+					noSymbolDialog.show();
+					return;
+				}
+
+				var totalW = w + (ww * 1.5);
+				var totalU = u + (uu * 1.5);
+				var totalB = b + (bb * 1.5);
+				var totalR = r + (rr * 1.5);
+				var totalG = g + (gg * 1.5);
+				var total = totalW + totalU + totalB + totalR + totalG;
+
+				var plains = (totalW / total) * dom.byId("totalLands").value;
+				var island = (totalU / total) * dom.byId("totalLands").value;
+				var swamp = (totalB / total) * dom.byId("totalLands").value;
+				var mountain = (totalR / total) * dom.byId("totalLands").value;
+				var forest = (totalG  / total) * dom.byId("totalLands").value;
+
+				dom.byId("numPlains").innerHTML = "Plains: " + plains;
+				dom.byId("numIslands").innerHTML = "Islands: " + island;
+				dom.byId("numSwamps").innerHTML = "Swamps: " + swamp;
+				dom.byId("numMountains").innerHTML = "Mountains: " + mountain;
+				dom.byId("numForests").innerHTML = "Forests: " + forest;
+			});
 		});
 	}
 	else {
--- src/app/widgets/ManaSymbolQuantity.js
+++ src/app/widgets/ManaSymbolQuantity.js
@@ -2,17 +2,25 @@
  * Used to specify the number of mana symbol breakdown in a deck.
  */
 define([
-    'dojo/_base/declare',
-    'dojo/dom-construct',
-    'dojo/parser',
-    'dojo/ready',
-    'dijit/_WidgetBase',
-    'dijit/_TemplatedMixin',
+	'dojo/_base/declare',
+	'dijit/_WidgetBase',
+	'dijit/_TemplatedMixin',
 	'dijit/_WidgetsInTemplateMixin',
-	'dojo/text!./templates/ManaSymbolQuantity.html'
-], function (declare, domConstruct, parser, ready, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template) {
+	'dojo/text!./templates/ManaSymbolQuantity.html',
+	'dijit/form/NumberSpinner'
+], function (declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin, template) {
 	return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
-        templateString: template,
-		color: ''
+		templateString: template,
+		color: '',
+
+		getSingleSymbolQuantity: function() {
+			return this.singleSymbolQuantity.get("value");
+		},
+
+		getDoubleSymbolQuantity: function() {
+			return this.doubleSymbolQuantity.get("value");
+		}
+
+		
 	});
 });
